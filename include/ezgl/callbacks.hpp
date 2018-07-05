64046a3e5c843bfd51836cc328d45381f652e37d
Mario Badr <mario.badr@outlook.com>
2018-07-05 15:09:35 -0400

    Add CMake macros to create glib resources
    Taken from: https://github.com/Makman2/GCR_CMake. This is version 3.3.
    The code is licensed under the GPL v3.

--- include/ezgl/callbacks.hpp
+++ include/ezgl/callbacks.hpp
@@ -9,25 +9,6 @@
 
 namespace ezgl {
 
-/**
- * Settings to configure the window.
- */
-struct window_settings {
-  /**
-   * The title of the window shown at the top.
-   */
-  std::string title = "My Window";
-
-  /**
-   * The width of the window in pixels.
-   */
-  int width = 640;
-
-  /**
-   * The height of the window in pixels.
-   */
-  int height = 480;
-};
 
 /**
  * The prototype of a function to be called on draw events.
@@ -43,21 +24,6 @@ using draw_callback_fn = void (*)(graphics g, int width, int height);
 inline void draw_nothing(graphics, int, int)
 {
 }
-
-/**
- * Settings to configure how graphics are drawn.
- */
-struct graphics_settings {
-  /**
-   * The function to call on draw events.
-   */
-  draw_callback_fn draw_callback = draw_nothing;
-
-  /**
-   * The background colour of the window.
-   */
-  colour background = WHITE;
-};
 
 /**
  * The prototype of a function to be called when a key is pressed.
@@ -95,24 +61,6 @@ inline void ignore(GdkEventButton *)
 {
 }
 
-/**
- * Settings to configure how to respond to input.
- */
-struct input_settings {
-  bool track_mouse_motion = false;
-  key_press_callback_fn key_press_callback = ignore;
-  mouse_move_callback_fn mouse_move_callback = ignore;
-  mouse_click_callback_fn  mouse_click_callback = ignore;
-};
-
-/**
- * A bundle of all ezgl settings.
- */
-struct settings {
-  window_settings window;
-  graphics_settings graphics;
-  input_settings input;
-};
 }
 
 #endif //EZGL_SETTINGS_HPP
