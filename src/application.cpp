d952f2e60ab5d53bfe1b9ca6588df966bb51985a
Mario Badr <mario.badr@outlook.com>
2018-08-10 13:18:02 -0400

    Cleanup GTK builder and document memory handling

--- src/application.cpp
+++ src/application.cpp
@@ -13,13 +13,6 @@ void application::startup(GtkApplication *, gpointer user_data)
     g_error("%s.", error->message);
   }
 
-  // Make sure that the window_id given exists in the GTK Builder.
-  GObject *window = gtk_builder_get_object(ezgl_app->m_builder, ezgl_app->m_window_id.c_str());
-  if(window == nullptr) {
-    g_error("XML resource does not contain a GTK window with the name %s.",
-        ezgl_app->m_window_id.c_str());
-  }
-
   g_info("application::startup successful.");
 }
 
@@ -27,9 +20,8 @@ void application::activate(GtkApplication *, gpointer user_data)
 {
   auto ezgl_app = static_cast<application *>(user_data);
 
-  // Retrieve the window from the GTK Builder.
-  GObject *window = gtk_builder_get_object(ezgl_app->m_builder, ezgl_app->m_window_id.c_str());
-  // Add the window to our GTK Application.
+  // The main parent window needs to be explicitly added to our GTK application.
+  GObject *window = ezgl_app->get_object(ezgl_app->m_window_id.c_str());
   gtk_application_add_window(ezgl_app->m_application, GTK_WINDOW(window));
 
   if(ezgl_app->m_register_callbacks != nullptr) {
@@ -38,9 +30,8 @@ void application::activate(GtkApplication *, gpointer user_data)
     g_warning("No user-defined callbacks have been registered.");
   }
 
-  // Retrieve the drawing area from the GTK Builder.
-  GObject *canvas = gtk_builder_get_object(ezgl_app->m_builder, ezgl_app->m_canvas_id.c_str());
-  // Enable mouse events for the drawing area.
+  // GtkDrawingArea objects need mouse button button presses enabled explicitly.
+  GObject *canvas = ezgl_app->get_object(ezgl_app->m_canvas_id.c_str());
   gtk_widget_add_events(GTK_WIDGET(canvas), GDK_BUTTON_PRESS_MASK);
 
   g_info("application::activate successful.");
@@ -54,14 +45,18 @@ application::application(char const *main_ui_resource, char const *window_id, ch
     , m_canvas_id(canvas_id)
     , m_register_callbacks(nullptr)
 {
-  // connect our static functions application::{startup, activate} to their callbacks
-  // we also pass 'this' object as the userdata so that we can use it in our static function
+  // Connect our static functions application::{startup, activate} to their callbacks. We pass 'this' as the userdata
+  // so that we can use it in our static functions.
   g_signal_connect(m_application, "startup", G_CALLBACK(startup), this);
   g_signal_connect(m_application, "activate", G_CALLBACK(activate), this);
 }
 
 application::~application()
 {
+  // GTK uses reference counting to track object lifetime. Since we called *_new() for our application and builder, we
+  // need to unreference them. This should set their reference count to 0, letting GTK know that they should be cleaned
+  // up in memory.
+  g_object_unref(m_builder);
   g_object_unref(m_application);
 }
 
@@ -72,7 +67,13 @@ void application::register_callbacks_with(setup_callbacks_fn fn)
 
 GObject *application::get_object(gchar const *name) const
 {
-  return gtk_builder_get_object(m_builder, name);
+  // Getting an object from the GTK builder does not increase its reference count.
+  GObject *object = gtk_builder_get_object(m_builder, name);
+  if(object == nullptr) {
+    g_error("Could not find a GUI object with the name %s.", name);
+  }
+
+  return object;
 }
 
 int application::run(int argc, char **argv)
