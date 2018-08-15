f7464b95718c8f69bc7704d84f092f031adeeee7
Mario Badr <mario.badr@outlook.com>
2018-08-15 16:55:41 -0400

    Fix scaling algorithm -- still not centred

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,19 +1,9 @@
 #include "ezgl/camera.hpp"
 
+#include <cmath>
+#include <cstdio>
+
 namespace ezgl {
-
-scale_transform
-calculate_scale(int screen_width, double view_width, int screen_height, double view_height)
-{
-  scale_transform scale{};
-
-  scale.x = screen_width / view_width;
-
-  // Cartesian coordinates has y increasing from the origin, but Cairo is the opposite, so we use a negative height.
-  scale.y = screen_height / -view_height;
-
-  return scale;
-}
 
 camera::camera(rectangle bounds) : m_view(bounds)
 {
@@ -30,30 +20,16 @@ void camera::update_screen(int width, int height)
 
 void camera::update_view(rectangle view)
 {
-  scale_transform const scale =
-      calculate_scale(m_screen_width, view.width(), m_screen_height, view.height());
+  double x_scale = m_screen_width / view.width();
+  double y_scale = m_screen_height / view.height();
 
-  // Maintain the aspect ratio by resizing the view.
-  if(scale.x <= scale.y) {
-    double const multiplier = scale.y / scale.x;
-    double const height = view.height();
-
-    double const y_top = view.top() - height * (multiplier - 1) * 0.5;
-    double const y_bottom = view.bottom() + height * (multiplier - 1) * 0.5;
-
-    m_view = rectangle({view.left(), y_bottom}, {view.right(), y_top});
+  if(x_scale * view.height() > m_screen_height) {
+    m_world_to_screen.x = y_scale;
+    m_world_to_screen.y = -y_scale;
   } else {
-    double const multiplier = scale.x / scale.y;
-    double const width = view.width();
-
-    double const x_left = view.left() - width * (multiplier - 1) * 0.5;
-    double const x_right = view.right() + width * (multiplier - 1) * 0.5;
-
-    m_view = rectangle({x_left, view.bottom()}, {x_right, view.top()});
+    m_world_to_screen.x = x_scale;
+    m_world_to_screen.y = -x_scale;
   }
-
-  m_world_to_screen =
-      calculate_scale(m_screen_width, m_view.width(), m_screen_height, m_view.height());
 }
 
 point2d camera::world_to_screen(point2d world_coordinates) const
