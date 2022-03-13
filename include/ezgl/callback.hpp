d857e9bac80bf63eda1bec1fc199717a1a287d25
Vaughn Betz <vaughn@eecg.utoronto.ca>
2022-03-13 17:19:40 -0400

    Fixed copyright header typo

--- include/ezgl/callback.hpp
+++ include/ezgl/callback.hpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Authors: Mario Badr, Sameh Attia ,Tanner Young-Schultz and Vaughn Betz
+ * Authors: Mario Badr, Sameh Attia, Tanner Young-Schultz and Vaughn Betz
  */
 
 #ifndef EZGL_CALLBACK_HPP
