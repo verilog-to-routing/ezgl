f184a1d9a94d353d28fd088dfc714d5347311e22
Oleksandr <apivovarov@quicklogic.com>
2026-04-08 08:44:49 +0300

    checkpoint

--- examples/renderer-stress-bench/renderer-stress-bench.cpp
+++ examples/renderer-stress-bench/renderer-stress-bench.cpp
@@ -260,9 +260,8 @@ struct TestCase {
   const char          *output_file;
 };
 
-// TODO: make max fast shader for drawing thin line
 static const TestCase TESTS[] = {
-  //{ "variadic lines   ", draw_lines_variadic,         200'000'000, "bench_lines_variadic.png"    },
+  { "variadic lines   ", draw_lines_variadic,         1'000'000, "bench_lines_variadic.png"    },
 //  { "solid lines      ", draw_lines_solid,           200'000'000, "bench_lines_solid.png"       },
 //  { "variadic lines   ", draw_lines_variadic,         1'000'000, "bench_lines_variadic.png"    },
   // { "variadic lines   ", draw_lines_variadic,         400'000'000, "bench_lines_variadic.png"    },
