a80f7d5934ec87853a85d376bb02262b0328d015
Oleksandr <apivovarov@quicklogic.com>
2026-04-17 19:39:22 +0300

    kTileGridDimension set to 16

--- include/ezgl/qt/rhi_renderer.hpp
+++ include/ezgl/qt/rhi_renderer.hpp
@@ -113,7 +113,7 @@ public:
     void flush_mvp_only();
 
 private:
-    static constexpr int kTileGridDimension = 32;
+    static constexpr int kTileGridDimension = 16;
 
     struct TileThinLineBatch {
         StyleKey               style_key = 0;
